048b53254ecacaad3d15a5fceef4fe852d68bf41
Clayton <127148272+Clay2002@users.noreply.github.com>
2023-04-26 18:01:44 -0400

    layout and hove effect testing
    This is around 10 mins into this project. Just some very basic stuff. going to work on it for around 1:30 mins and see how far I get.

---
+++ tic-tac-toe.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+function onhover(){
+
+
+
+
+var onhover = document.getElementById("cell1");
+onhover.style.backgroundColor = 'red';
+
+
+
+
+}
+
+
+
+function offhover(){
+
+
+
+
+    var offhover = document.getElementById("cell1");
+    offhover.style.backgroundColor = 'green';
+    
+    
+    
+    
+    }
